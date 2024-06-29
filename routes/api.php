824fa8837143cfec6c6d3aa1940173d77c17ab9d
Abdul Kholik <engineertekno@gmail.com>
2024-06-29 23:50:31 +0700

    Membuat Restful API di Laravel 8

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::resource('/post', PostController::class);
