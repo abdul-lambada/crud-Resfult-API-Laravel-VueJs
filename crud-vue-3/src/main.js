5a1aa4cb9acab4a649a3b94dfaf7be08d8b95af2
Abdul Kholik <engineertekno@gmail.com>
2024-06-30 01:01:12 +0700

    Instalasi dan Konfigurasi Vue Router, Axios dan Bootsrtap

--- crud-vue-3/src/main.js
+++ crud-vue-3/src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//import router
+import router from './router'
+
+//import Bootstrap, Popper, Jquery
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/dist/jquery.min.js'
+import 'popper.js/dist/popper.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+// createApp(App).mount('#app')
+
+const app = createApp(App)
+
+//use vue router
+app.use(router)
+
+app.mount('#app')
