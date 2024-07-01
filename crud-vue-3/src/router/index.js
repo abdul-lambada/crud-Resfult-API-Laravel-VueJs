ea804b8804a5cb0599625e88731f6cab84f3805d
Abdul Kholik <engineertekno@gmail.com>
2024-07-01 22:07:57 +0700

    Input Data di VueJs

--- crud-vue-3/src/router/index.js
+++ crud-vue-3/src/router/index.js
@@ -6,30 +6,24 @@ const routes = [
     {
         path: '/',
         name: 'post.index',
-        component: () => import(
-            /* webpackChunkName: "post.index" */ '@/views/post/Index.vue'
-        )
+        component: () => import( /* webpackChunkName: "post.index" */ '@/views/post/Index.vue')
     },
     {
         path: '/create',
         name: 'post.create',
-        component: () => import(
-            /* webpackChunkName: "post.create" */ '@/views/post/Create.vue'
-        )
+        component: () => import( /* webpackChunkName: "post.create" */ '@/views/post/Create.vue')
     },
     {
-        path: '/edit/id',
+        path: '/edit/:id',
         name: 'post.edit',
-        component: () => import(
-            /* webpackChunkName: "post.edit" */ '@/views/post/Edit.vue'
-        )
+        component: () => import( /* webpackChunkName: "post.edit" */ '@/views/post/Edit.vue')
     }
 ]
 
 //create router
 const router = createRouter({
     history: createWebHistory(),
-    routes // config routes
+    routes  // config routes
 })
 
 export default router
